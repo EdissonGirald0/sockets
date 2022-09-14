db38430384aeeb413b270ebb850910394240b65e
edisson giraldo <edisson.giraldo@hotmail.com>
2022-09-13 21:57:22 -0500

    socket multiCliente

--- src/sockets/cliente/Cliente.java
+++ src/sockets/cliente/Cliente.java
@@ -2,6 +2,7 @@ package sockets.cliente;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
@@ -17,30 +18,65 @@ public class Cliente extends Conexion {
     public void startClient() //Método para iniciar el cliente
     {
         try {
-            //Flujo de datos hacia el servidor
-            salidaServidor = new DataOutputStream(cs.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos el "lector"
+            System.out.println("Por favor seleccione una operación");
+            System.out.println("1. Crear cuenta");
+            System.out.println("2. Consultar Saldo");
 
-            System.out.println("Por favor ingrese su numero de cuenta");//Se pide un dato al usuario
-
-            String cuenta = br.readLine(); //Se lee el nombre con readLine() que retorna un String con el dato
-
-            System.out.println("cuenta " + cuenta + ". Por favor ingrese un valor");//Se pide otro dato al usuario
-
-            String entrada = br.readLine(); //Se guarda la entrada en una variable
-
-            //Nótese que readLine siempre retorna String y la clase BufferedReader...
-            //no tiene un método para leer enteros, así que debemos convertirlo.
-            int valor = Integer.parseInt(entrada);//Se transforma la entrada anterior en un entero
-            //Si el usuario ingresó solo números funcionará bien, de lo contrario generará una excepción
-
-            System.out.println(cuenta + ","+ valor); //Operacion numerica con la edad
-            
-                //Se escribe en el servidor usando su flujo de datos
-                salidaServidor.writeUTF(cuenta +","+ valor);
-       
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String opcion = br.readLine();
+            if(opcion.equals("1")){
+                this.createAccount();
+            }else if(opcion.equals("2")){
                 
+                this.queryAccount();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void createAccount() //Método para iniciar el cliente
+    {
+        try {
+                           
+            DataOutputStream salida = new DataOutputStream(cs.getOutputStream());
+            DataInputStream entrada = new DataInputStream(cs.getInputStream());
+            BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+            salida.writeUTF("createAccount");
+            System.out.println("Por favor ingrese la cuenta");
+            String cuenta = lector.readLine();
+            System.out.println("Por favor ingrese el saldo");
+            String valor = lector.readLine();
+            salida.writeUTF(cuenta + ',' + valor);
+            String resultado = entrada.readUTF();
+            System.out.println(resultado);
 
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                cs.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void queryAccount() //Método para iniciar el cliente
+    {
+        try {
+            DataOutputStream salida = new DataOutputStream(cs.getOutputStream());
+            DataInputStream entrada = new DataInputStream(cs.getInputStream());
+            BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+            salida.writeUTF("queryAccount");
+            System.out.println("Por favor ingrese la cuenta");
+            String cuenta = lector.readLine();
+            salida.writeUTF(cuenta);
+            String resultado = entrada.readUTF();
+            if (resultado.isEmpty()){
+                System.out.println("Cuenta no encontrada");
+            }else{
+                System.out.println(resultado);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
--- src/sockets/servidor/Servidor.java
+++ src/sockets/servidor/Servidor.java
@@ -1,14 +1,14 @@
 package sockets.servidor;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Scanner;
 import sockets.conexion.Conexion;
 
 public class Servidor extends Conexion //Se hereda de conexión para hacer uso de los sockets y demás
@@ -20,51 +20,47 @@ public class Servidor extends Conexion //Se hereda de conexión para hacer uso d
 
     public void startServer()//Método para iniciar el servidor
     {
-        try {
-            System.out.println("Esperando..."); //Esperando conexión
-
-            cs = ss.accept(); //Accept comienza el socket y espera una conexión desde un cliente
-
-            System.out.println("Cliente en línea");
-
-            //Se obtiene el flujo de salida del cliente para enviarle mensajes
-            salidaCliente = new DataOutputStream(cs.getOutputStream());
-
-            //Se le envía un mensaje al cliente usando su flujo de salida
-            salidaCliente.writeUTF("Petición recibida y aceptada");
-
-            //Se obtiene el flujo entrante desde el cliente
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-
-            while ((mensajeServidor = entrada.readLine()) != null) //Mientras haya mensajes desde el cliente
-            {
-                //Se muestra por pantalla el mensaje recibido
-                System.out.println(mensajeServidor);
-                scribeTxt();
-            }
-            
-
-            System.out.println("Fin de la conexión");
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
+        while (true) {
             try {
-                cs.close();//Se finaliza la conexión con el cliente
-            } catch (IOException ex) {
-                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Esperando..."); //Esperando conexión
+                cs = ss.accept();
+                DataOutputStream salida = new DataOutputStream(cs.getOutputStream());
+                DataInputStream entrada = new DataInputStream(cs.getInputStream());
+                String operacion = entrada.readUTF();
+                if (operacion.equals("createAccount")){
+                    String user_data = entrada.readUTF();
+                    boolean resultado = scribeTxt(user_data);
+                    if (resultado){
+                        salida.writeUTF("OK");
+                    } else {
+                        salida.writeUTF("NO-OK");
+                    }
+                }else if (operacion.equals("queryAccount")){
+                    String user_data = entrada.readUTF();
+                    String resultado = searchAccount(user_data);
+                    salida.writeUTF(resultado);
+                }
+                System.out.println("Fin de la conexion");
+    
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    cs.close();//Se finaliza la conexión con el cliente
+                } catch (IOException ex) {
+                    Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
 
     }
 
-    public void scribeTxt() {
+    public boolean scribeTxt(String inputData) {
         BufferedWriter bw = null;
         FileWriter fw = null;
-
+        boolean resultado = false;
         try {
-            String data = mensajeServidor;
-            File file = new File("archivo.txt");
+            File file = new File("base.csv");
             // Si el archivo no existe, se crea!
             if (!file.exists()) {
                 file.createNewFile();
@@ -72,10 +68,12 @@ public class Servidor extends Conexion //Se hereda de conexión para hacer uso d
             // flag true, indica adjuntar información al archivo.
             fw = new FileWriter(file.getAbsoluteFile(), true);
             bw = new BufferedWriter(fw);
-            bw.write(data);
-            System.out.println("información agregada!");
+            bw.write(inputData);
+            bw.write('\n');
+
         } catch (IOException e) {
             e.printStackTrace();
+            return resultado;
         } finally {
             try {
                 //Cierra instancias de FileWriter y BufferedWriter
@@ -85,9 +83,31 @@ public class Servidor extends Conexion //Se hereda de conexión para hacer uso d
                 if (fw != null) {
                     fw.close();
                 }
+                resultado = true;
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
+        return resultado;
+    }
+
+    public String searchAccount(String Account){
+        String resultado = "";
+        File base = new File("base.csv");
+        try{
+            Scanner lector = new Scanner(base);
+            while(lector.hasNextLine()){
+                String[] line = lector.nextLine().split(",", 2);
+                if (line[0].equals(Account)){
+                    resultado = line[1];
+                }
+            }
+            lector.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    
+        return resultado;
+
     }
 }
